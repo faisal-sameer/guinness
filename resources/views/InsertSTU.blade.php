5067a29379c29470e889492e0a4b054b8c43c716
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2020-01-26 15:45:28 +0300

    view

--- resources/views/InsertSTU.blade.php
+++ resources/views/InsertSTU.blade.php
@@ -4,10 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    
+    <style>
+    #sty{
+      margin-top:5%;
+  margin-left: 30%;
+
+    } 
+     
+    </style>
 </head>
 <body>
-    
+
+<div id="sty" >
+School Name
+<input type="text"name ="school_name" > 
+
+
+
+Student Name
+<input type="text"name ="student_name" >
+
+
+<button  class="btn success">
+save
+
+
+</button>
+</div>
+ 
 
     
 </body>
