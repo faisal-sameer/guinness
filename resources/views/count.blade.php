2e6548b707c22fc423fa328663f13ab89f059ee4
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2020-01-27 16:13:03 +0300

    count

--- resources/views/count.blade.php
+++ resources/views/count.blade.php
@@ -5,16 +5,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<style>
+.main_container {
+  height: 46px;
+  width: auto;
+  padding: 3px;
+  margin-left: 40%;
+  margin-top: 10%;
+  
+  max-width: 300px;
+  background-color: #555555;
+  align-content: center;
+}
+.container_inner {
+  height: auto;
+  border: none;
+  background-color: #555555;
+  max-width: 290px;
+  vertical-align: center;
+  padding-top: 12px;
+  padding-left: 10px;
+  align-content: center;
+}
+ .num_tiles {
+  width:30px;
+  height: 30px;
+  background-color: #888888;
+  color: #ffffff;
+  font-size: 22px;
+  text-align: center;
+  line-height: 20px;
+  padding: 3px;
+  margin: 1.5px;
+  font-family: verdana;
+  vertical-align: center;
+}
+h2{
+    margin-left:42%;
+    margin-top:7%;
+
+
+}
+
+</style>
+
+
    </head>
 <body>
 
-<table class="borderless table inline-table  h4">
+<h2 class="attend"> Number of attendees
+</h2>
+<div class="main_container" id="id_main_container">
 
-<label> 
+      <div class="container_inner" id="display_div_id">
+     
+<label class="num_tiles"> 
 {{$count->count()}}
 </label>
+      </div>
+    </div>
 
- </table>
+
 
 
 </body>
