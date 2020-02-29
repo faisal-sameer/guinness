b553a8f0bbe78c7b0d80f82956c73270bef06011
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-02-29 17:56:39 +0300

    8puzzle_Five_postion

--- app/Http/Controllers/Puzzle.php
+++ app/Http/Controllers/Puzzle.php
@@ -4,59 +4,122 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class Puzzle extends Controller
 {
-  
 
-    public function puzzle8(){
-
-		$gole =    [1,2,3
-					,4,5,6,
-					7,8,0];
-		$mystate = [1,7,2
-					,4,3,5
-					,6,0,8];
-		$moves = 0 ;
-		$bad = 0 ;
-		$step = " ";
-		$normal = [1,2,3,4,5,6,7,8,0];
-		$new = [];
-
-		for ($i=0; $i <=8 ; $i++) {
-			if($mystate[$i] == $gole[$i]){
-				$moves++;
-
+	
+//	var $key = array_search(0,$mystate);
+    public function puzzle8( ){
+		 $goal =    [1,2,3
+		,4,5,6,
+		7,8,0];
+		 $mystate = [1,2,3
+		,4,5,0
+		,7,8,6];
+		 $steps = 0 ;
+		 $bad = 0 ;
+		 $step = " ";
+		 $normal = [1,2,3,4,5,6,7,8,9];
+		 $fringe = [];
+		 $depth = 0 ;
+		 $new = [] ;
+		 $key = 0;
+		 $string = " ";
+		for($i = 0 ; $i<=8 ; $i ++){
+			if($mystate[$i] == $goal[$i]){
+				$steps ++ ;
 			}else{
-				$bad ++ ;
-			}
-			
-			if($mystate[$i]==0){
-				$key = array_search(0,$mystate);
-				if($key == 0 || $key == 2 || $key == 6 || $key == 8){
-					$step = $step . "Tow Steps " . $key  ;
-
-				}else if ($key == 1 || $key == 3 || $key == 5 || $key == 7){
-					$step = $step . "Three Steps " . $key ;
-					if($key == 7 ){
-						$new = $mystate;
-						$new [7] = $mystate[8];
-						$new [8] = $mystate [7];
+				$bad++ ;
+				for($j = 0 ; $j <= 8 ; $j++){
+				$key = array_search(0,$mystate);// zero postion 
+				// need var for root and other for next root 
+				// and for loop for each postion to search all the nodes in the that postions 
+				$root = $key ; 
+				if($key == 5){// zero in postion 5
+					$fringe[] = 2;
+					$fringe[] = 4;
+					$fringe[] = 8;
+					for ($five = 0 ; $five <= 2 ; $five++){
+				
+					$root = $fringe[$five] ;
+					$new = $mystate;
+					if($fringe[0] == $root  ){
+					$new[2] = $mystate[5];
+					$new[5]= $mystate[2] ;
+				
+				//	$mystate = $new ;
+					}else if ($fringe[1] == $root) {
+						$new[4] = $mystate[5];
+						$new[5]= $mystate[4] ;
+				//		$mystate = $new ;
+					}else if ($fringe[2] == $root) {
+						$new[8] = $mystate[5];
+						$new[5]= $mystate[8] ;
+				//		$mystate = $new ;
+				
 					}
 					
 				}
+					$depth ++ ;
+				}/*else if ($key == 2){ // zero in postion 2 
+					$fringe[] = 1;
+					$fringe[] = 5;
+					$new = $mystate;
+					$new[2] = $mystate[5];
+					$new[5]= $mystate[2] ;
+					$mystate = $new ;
+					$depth ++ ;
+				}else if ($key == 1){ // zero in postion 1 
+					$fringe[] = 0;
+					$fringe[] = 2;
+					$fringe[] = 4;
+					$new = $mystate;
+					$new[1] = $mystate[2];
+					$new[2]= $mystate[1] ;
+					$mystate = $new ;
+					$depth ++ ;
+				}*/
+
+				
+
+			}
+		}
+			if($steps == 8 ){
+				$string = "found Goal ";
 			}
 
 		}
+		$arr = Array('steps'=>$steps , 'bad'=>$bad  , 'key'=>$key , 'mystate'=>$mystate, 'fringe'=>$fringe , 'string'=>$string , 'depth'=>$depth);
 
-		
-		
-		
-		
-		$arr = Array('moves'=>$moves , 'bad'=>$bad , 'step'=>$step , 'key'=>$key ,  'new'=>$new);
 
         return view('puzzle' , $arr);
 
 
+		
+		//$arr = Array('moves'=>$moves , 'bad'=>$bad , 'step'=>$step , 'key'=>$key ,  'new'=>$new , 'seven'=>$seven);
 
-    }
+       // return view('puzzle' , $arr);
+	}
+	protected function POS ($goal , $mystate , $steps , $fringe , $depth){
+		for($i = 0 ; $i<=8 ; $i ++){
+			if($mystate[$i] == $goal[$i]){
+				$steps ++ ;
+			}else{
+				$bad++ ;
+			}
+			if($steps == 8 ){
+				$text = "found Goal ";
+				
+				
+			}
+
 }
+return  $text;
+}
+}
+
+
+
+
+
